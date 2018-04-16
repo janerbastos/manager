2e8c5e56386c079e1bab15faeafa4ebc58a3f9d9
Janer B. Melo <janerbastos@gmail.com>
2018-04-16 12:24:14 -0300

    Configurações
    Melhorias na camada de modelo

--- models.py
+++ models.py
@@ -7,31 +7,31 @@ from mamba.models import Site, ContentType
 class UserSite(models.Model):
     user = models.ForeignKey(User)
     site = models.ForeignKey(Site)
-    grupo = models.ManyToManyField('Grupo', related_name='user_grupos')
+    grupo = models.ManyToManyField('Grupo', related_name='rel_grupos_sites')
 
     class Meta:
         unique_together = (("user", "site"),)
 
 
 class Grupo(models.Model):
-    grupo_name = models.CharField(max_length=60, unique=True)
+    nome = models.CharField(max_length=60, unique=True)
 
     def __unicode__(self):
-        return self.grupo_name
+        return self.nome
 
 
 class GrupoPapel(models.Model):
-    grupo = models.ForeignKey('Grupo', related_name='grupos')
-    papeis = models.ManyToManyField('Papel', related_name='grupo_papeis')
+    grupo = models.ForeignKey('Grupo', related_name='rel_grupos_papeis')
+    papeis = models.ManyToManyField('Papel', related_name='rel_papeis')
 
     def __unicode__(self):
-        return self.grupo.grupo_name
+        return self.grupo.nome
 
 
 class Papel(models.Model):
-    papel_name = models.CharField(max_length=50)
+    nome = models.CharField(max_length=50)
     cod_name = models.SlugField(max_length=50, null=True)
-    content_type = models.ForeignKey(ContentType, related_name='content_papel', null=True)
+    content_type = models.ForeignKey(ContentType, related_name='rel_contents', null=True)
 
     def __unicode__(self):
-        return self.papel_name
+        return self.nome
